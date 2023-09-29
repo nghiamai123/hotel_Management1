bf77a2c6ad21dccd3da035dfd5560b8769b05a0d
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-09-30 00:22:27 +0700

    change positon and avata

---
+++ js/hotel_home_page.js
@@ -0,0 +1,5 @@
+function dangnhap(){
+     document.getElementById("sign").style.display = 'none';
+     document.getElementById("regis").style.display = 'none';
+     document.getElementById("avata").style.display = 'block';
+}
