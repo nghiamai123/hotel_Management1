5e754bf981b1db371c54379d21df924e28dd5924
Duong Thi Hong Lam <lam.duong25@student.passerellesnumeriques.org>
2023-10-06 21:12:50 +0700

    add homepage.js

--- js/hotel_home_page.js
+++ js/hotel_home_page.js
@@ -1,14 +1,29 @@
-var nameArray = [];
-var emailArray=[];
-var passwordArray=[];
-var dateArray=[];
-var genderArray =[];
+var nameArray = [
+    "Duong Thị Hồng Lam",
+    "Trần Thị Việt Mỹ"
+    
+];
+var emailArray=[
+    "honglamhk@gmail.com"
+];
+var passwordArray=[
+    "lamlam"
+];
+var dateArray=[
+    "2023-10-01"
+];
+var genderArray =[
+    "male"
+];
 var phoneArray =[];
 
 function register(){
     var name = document.getElementById("registerName").value;
     var email = document.getElementById("registerEmail").value;
     var date = document.getElementById("auth-form__date").value;
+    var selectElement = document.getElementById("auth-form__gender");
+    var gender = selectElement.value;
+    var phone = document.getElementById("registerPhone");
     var password = document.getElementById("registerPassword").value;
     var passwordConfirm = document.getElementById("registerConfirmPassword").value;
     if (name == ""){
@@ -18,6 +33,17 @@ function register(){
     else if (email == ""){
         alert("Email required.");
         return ;
+    }
+    else if (date == ""){
+        alert("date required.");
+        return ;
+    }
+    else if(gender == ""){
+        alert("gender requirment.")
+        return;
+    }
+    else if (phone ==""){
+        alert("Phone required.")
     }
     else if (password == ""){
         alert("Password required.");
@@ -31,22 +57,39 @@ function register(){
         alert("Password don't match retype your Password.");
         return;
     }
-    else if(emailArray.indexOf(email) == -1){
+    else if(emailArray.indexOf(email) == -1){ //kiểm trả xem gia
+        nameArray.push(name);
         emailArray.push(email);
+        dateArray.push(date);
+        genderArray.push(gender);
+        phoneArray.push(phone);
         passwordArray.push(password);
-        localStorage.setItem('listEmail',JSON.stringify(emailArray) );
-        localStorage.setItem('listPassword',JSON.stringify(passwordArray) );
         alert(email + "  Thanks for registration. \nTry to login Now");
-
-        document.getElementById("re").value ="";
-        document.getElementById("rp").value="";
-        document.getElementById("rrp").value="";
+        name = document.getElementById("registerName").value='';
+        email = document.getElementById("registerEmail").value='';
+        date = document.getElementById("auth-form__date").value='';
+        selectElement = document.getElementById("auth-form__gender")='';
+        gender = selectElement.value='';
+        phone = document.getElementById("registerPhone")='';
+        password = document.getElementById("registerPassword").value='';
+        passwordConfirm = document.getElementById("registerConfirmPassword").value='';
+        localStorage.setItem('listName',JSON.stringify(nameArray) );
+        localStorage.setItem('listEmail',JSON.stringify(emailArray) );
+        localStorage.setItem('listPhone',JSON.stringify(phoneArray) );
+        localStorage.setItem('listDate',JSON.stringify(dateArray) );
+        localStorage.setItem('listGender',JSON.stringify(genderArray) );
+        localStorage.setItem('listPassword',JSON.stringify(passwordArray) );
+        
     }
     else{
         alert(email + " is already register.");
         return ;
     }
 }
+
+
+
+
 function login(){
     var email = document.getElementById("loginEmail").value;
     var password = document.getElementById("loginPassword").value;
@@ -72,26 +115,11 @@ function login(){
     else {
         alert(email + " yor are login Now \n welcome to our website.");
 
-        document.getElementById("se").value ="";
-        document.getElementById("sp").value="";
+        document.getElementById("loginEmail").value ="";
+        document.getElementById("loginPassword").value="";
         return ;
     }
 
 }
-function forgot(){
-    event.preventDefault();
 
-    var email = document.getElementById("fe").value;
 
-    if(emailArray.indexOf(email) == -1){
-        if (email == ""){
-            alert("Email required.");
-            return ;
-        }
-        alert("Email does not exist.");
-        return ;
-    }
-
-    alert("email is send to your email check it in 24hr. \n Thanks");
-    document.getElementById("fe").value ="";
-}
