2e65c4ff04d6e8fd0079adc7ecc5b9e38331c037
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-10-16 08:15:28 +0700

    add function share by mail

--- js/detail.js
+++ js/detail.js
@@ -26,3 +26,25 @@ fetch(baserooms)
         }
     });
 })
+// chức năng share qua mail.
+function shareViaEmail() {
+    var subject = " Try booking a room at YAWN YAWN now ";
+    var body = " Dear sir/madam \n" +
+    " I hope this email finds you well. I wanted to reach out to you about an exciting opportunity to book a room at YAWN YAWN, the premier hotel for ultimate relaxation and comfort. \n" +
+
+    " At YAWN YAWN, we offer luxurious accommodations, exceptional service, and a tranquil atmosphere that guarantees a peaceful stay. Whether you are on a business trip or looking for a rejuvenating getaway, our hotel provides the perfect environment for your needs. \n" +
+    
+    " To take advantage of this special offer, simply visit our website at http:hotel_home_page.html. Our friendly staff will be delighted to assist you in making a reservation and answer any questions you may have. \n" +
+    
+    " Do not miss out on this opportunity to experience the unparalleled comfort and hospitality of YAWN YAWN. Book your room today and treat yourself to a truly memorable stay. \n" +
+    
+    " We look forward to welcoming you as our valued guest. \n" +
+    
+    " Warm regards. "
+  
+    // Tạo URL mailto với tiêu đề và nội dung đã cho
+    var mailtoUrl = "mailto:?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+  
+    // Mở trình đánh thư điện tử mặc định với URL mailto
+    window.location.href = mailtoUrl;
+}
