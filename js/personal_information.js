e4688e5bf49895c70748477ce601d2d930956baa
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-10-16 10:54:54 +0700

    update change data json-server

--- js/personal_information.js
+++ js/personal_information.js
@@ -1,9 +1,9 @@
 const listUser1 = "http://localhost:3000/user";
+var userid = localStorage.getItem('listuser');
 fetch(listUser1)
 .then((res) => res.json())
 .then((data) => {
     data.forEach(element => {
-        var userid = localStorage.getItem('listuser');
         if (userid == element.id){
             document.getElementById("content_profile-username").innerHTML = element.name;
             document.getElementById("profile-date").innerHTML = element.age;
@@ -22,24 +22,42 @@ fetch(listUser1)
 })
 
 function change() {
-   document.getElementById("user_name").value = document.getElementById("name").value;
-   document.getElementById("email11").value = document.getElementById("email").value;
-   document.getElementById("user_phonenumber").value = document.getElementById("phone").value;
-   document.getElementById("user_password").value = document.getElementById("password").value;
-    // alert("change information success!!!")
+  document.getElementById("user_name").value = document.getElementById("name").value;
+  document.getElementById("email11").value = document.getElementById("email").value;
+  document.getElementById("user_phonenumber").value = document.getElementById("phone").value;
+  document.getElementById("user_password").value = document.getElementById("password").value;
+  const apiUrl = 'http://localhost:3000';
+  const tableName = 'user';
+  const recordId = userid;
+
+  const updatedData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    password: document.getElementById("password").value,
+    avata: JSON.parse(localStorage.getItem("avata")),
+    age: "2023-10-12",
+    gender: "Unidentify"
+  };
+
+  fetch(`${apiUrl}/${tableName}/${recordId}`, {
+    method: 'PUT', // Hoặc 'PATCH' nếu bạn chỉ muốn cập nhật một phần dữ liệu
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(updatedData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Data update failed.');
+      }
+      alert('The data has been updated successfully.');
+    })
+    .catch(error => {
+      alert('Error updating data:', error);
+    });
 }
-            
-// const uploadBtn = document.getElementById('profile-edit-avata');
-// const avatarInput = document.getElementById('avatar-input');
 
-// uploadBtn.addEventListener('click', function() {
-//   avatarInput.click();
-// });
-
-// avatarInput.addEventListener('change', function(event) {
-//   const file = event.target.files[0];
-//   // Xử lý tải lên file ở đây
-// });
 const uploadBtn = document.getElementById('profile-edit-avata');
 const avatarInput = document.getElementById('avatar-input');
 const previewImage = document.getElementById('preview-image');
