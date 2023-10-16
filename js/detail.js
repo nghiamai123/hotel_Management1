d5254babd0a603d99b632c0d57a973720b3a3b78
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-10-16 23:21:47 +0700

    fix return detail

--- js/detail.js
+++ js/detail.js
@@ -32,15 +32,15 @@ function shareViaEmail() {
     var body = " Dear sir/madam \n" +
     " I hope this email finds you well. I wanted to reach out to you about an exciting opportunity to book a room at YAWN YAWN, the premier hotel for ultimate relaxation and comfort. \n" +
 
-    " At YAWN YAWN, we offer luxurious accommodations, exceptional service, and a tranquil atmosphere that guarantees a peaceful stay. Whether you are on a business trip or looking for a rejuvenating getaway, our hotel provides the perfect environment for your needs. \n" +
+    "At YAWN YAWN, we offer luxurious accommodations, exceptional service, and a tranquil atmosphere that guarantees a peaceful stay. Whether you are on a business trip or looking for a rejuvenating getaway, our hotel provides the perfect environment for your needs. \n" +
     
-    " To take advantage of this special offer, simply visit our website at http:hotel_home_page.html. Our friendly staff will be delighted to assist you in making a reservation and answer any questions you may have. \n" +
+    "To take advantage of this special offer, simply visit our website at http:hotel_home_page.html. Our friendly staff will be delighted to assist you in making a reservation and answer any questions you may have. \n" +
     
-    " Do not miss out on this opportunity to experience the unparalleled comfort and hospitality of YAWN YAWN. Book your room today and treat yourself to a truly memorable stay. \n" +
+    "Do not miss out on this opportunity to experience the unparalleled comfort and hospitality of YAWN YAWN. Book your room today and treat yourself to a truly memorable stay. \n" +
     
-    " We look forward to welcoming you as our valued guest. \n" +
+    "We look forward to welcoming you as our valued guest. \n" +
     
-    " Warm regards. "
+    "Warm regards. "
   
     // Tạo URL mailto với tiêu đề và nội dung đã cho
     var mailtoUrl = "mailto:?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
