fabc1fa57c41392fbd7610b0eb5ffa5d0a6a5a47
Viet My <Ziettmy@gmail.com>
2023-10-19 09:38:09 +0700

    updates admin

--- js/admin.js
+++ js/admin.js
@@ -9,10 +9,44 @@ fetch(baserooms)
             <td>${data1.nameroom}</td>
             <td>${data1.type[0]}</td>
             <td>${data1.price}</td>
-            <td><button class="button">Sửa</button> <button class="button">Xóa</button></td>
+            <td>
+                <button class="button-icon">
+                    <i class="fa fa-cogs" aria-hidden="true" id="${data1.id}"></i>
+                </button>
+                <button class="button-icon">
+                    <i class="fa fa-trash" aria-hidden="true" id="${data1.id}" ></i>
+                </button>
+            </td>
         </tr>
         `
     
     })
-    document.getElementById("rooms-table").innerHTML = ` ${a.join("")}`
+    document.getElementById("rooms-table").innerHTML = `${a.join("")}`
+})
+const user= "http://localhost:3000/user";
+fetch(user)
+.then((res) => res.json())
+.then((data)=> {
+    var a = data.map((data2) => {
+        return`
+        <tr>
+            <th>${data2.id}</th>
+            <td>${data2.name}</td>
+            <td>${data2.password}</td>
+            <td>${data2.phone}</td>
+            <td>${data2.email}</td>
+            <td>none</td>
+            <td>
+                <button class="button-icon">
+                    <i class="fa fa-cogs" aria-hidden="true" id="${data2.id}"></i>
+                </button>
+                <button class="button-icon">
+                    <i class="fa fa-trash" aria-hidden="true" id="${data2.id}" ></i>
+                </button>
+            </td>
+        </tr>
+        `
+    
+    })
+    document.getElementById("user-table").innerHTML = `${a.join("")}`
 })
