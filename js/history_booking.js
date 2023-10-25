e66383e9b49b761d85f16340088255cca3925343
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-10-25 19:43:33 +0700

    add contacts

--- js/history_booking.js
+++ js/history_booking.js
@@ -4,16 +4,16 @@ const historybooking = `http://localhost:3000/order`;
 fetch(historybooking)
     .then((res) => res.json())
     .then(data => {
-        var hry = data.map((hry) =>
-        {
+        var hry = data.map((users) =>
+        { if (IDuser == users.user) {
             return `<tr>
-            <td>${hry.nameroom}</td>
-            <td>${hry.numbercard}</td>
-            <td>${hry.price}</td>
-            <td>${hry.chin}</td>
-            <td>${hry.chout}</td>
-            <td>${hry.status}</td>
-            <td>${hry.totalprice}</td>
+            <td>${users.nameroom}</td>
+            <td>${users.numbercard}</td>
+            <td>${users.price}</td>
+            <td>${users.chin}</td>
+            <td>${users.chout}</td>
+            <td>${users.status}</td>
+            <td>${users.totalprice}</td>
             <td>
                 <button class="button-icon" >
                     <i class="fa fa-cogs" aria-hidden="true"></i>
@@ -23,6 +23,7 @@ fetch(historybooking)
                 </button>
             </td>
         </tr>`
+        }
         })      
     document.getElementById("rooms-table").innerHTML = hry.join("");
 })
