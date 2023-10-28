eb84fa41bbbcf4be36a8c0a53f0e2832857cf933
Viet My <Ziettmy@gmail.com>
2023-10-28 21:06:13 +0700

    CRUD room and user

--- js/admin.js
+++ js/admin.js
@@ -73,23 +73,23 @@ fetch(rooms)
                 },
                 body: JSON.stringify(editRoom),
             })
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error("Cập nhật Room thất bại.");
-                    }
-                return response.json();
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Cập nhật Room thất bại.");
+                }
+            return response.json();
+            })
+            .then((editRoom) => {
+                // Xử lý phản hồi sau khi phòng đã được cập nhật thành công
+                console.log("You have successfully added the room: ", editRoom);
+                alert("You have successfully edit the room");
+                return;
+                
                 })
-                .then((editRoom) => {
-                    // Xử lý phản hồi sau khi phòng đã được cập nhật thành công
-                    console.log("You have successfully added the room: ", editRoom);
-                    alert("You have successfully edit the room")
-                    return;
-                   
-                  })
-                .catch((error) => {
-                    // Xử lý lỗi nếu có
-                    console.error("Lỗi khi cập nhật phòng:", error);
-                })
+            .catch((error) => {
+                // Xử lý lỗi nếu có
+                console.error("Lỗi khi cập nhật phòng:", error);
+            })
             
         }) 
         })  
@@ -101,7 +101,7 @@ fetch(rooms)
                 })
                 .then((response) => {
                     if (!response.ok) {
-                    throw new Error("Xóa người dùng thất bại.");
+                    throw new Error("Xóa người phòng thất bại.");
                     }
                     return response.json();
                 })
@@ -176,62 +176,172 @@ fetch(rooms)
         })
     }
 // edit room
-// function getEditId(event) {
-//     // Truy cập vào phần tử HTML cha (button)
-//     var button = event.target.parentElement;
-    
-//     // Lấy giá trị ID
-//     var idEdit = button.querySelector("i").id;
-//     console.log(idEdit);
-    
-//     for (let element in rooms){
-//         if (String(idEdit)==String(rooms.element.id)){
-//             document.getElementById("admin_edit_id_room")=rooms.id;
-//             document.getElementById("admin_edit_name_room")=rooms.nameroom;
-//             document.getElementById("admin_edit_price")=rooms.price;
-//             document.getElementById("admin_edit_quanity-bed")=rooms.type[0];
-//             document.getElementById("admin_edit_quanity-bath")=rooms.type[1];
-//             document.getElementById("admin_edit_quanity-wifi")=rooms.type[2];
 
-
-
-//             document.getElementById("admin_edit_id_room").setAttribute("disabled","disabled");
-//         }
-//     }
-//     // trả về đối tượng có id đó.
-//         // const objRoom= data.find(rooms => rooms.id === idEdit);
-        
-//         // console.log(objRoom);
-    
-// }
 // end ROom
 // MANAGE USER
     // show user
     const user= "http://localhost:3000/user";
     fetch(user)
-    .then((res) => res.json())
+    .then((response) => response.json())
     .then((data)=> {
         var a = data.map((data2) => {
             return`
             <tr>
                 <th>${data2.id}</th>
                 <td>${data2.name}</td>
+                <td>${data2.gender}</td>
                 <td>${data2.email}</td>
                 <td>${data2.phone}</td>
                 <td>${data2.password}</td>
+
                 <td>none</td>
                 <td>
                     <button class="button-icon">
-                        <i class="fa fa-cogs" aria-hidden="true"  data-bs-toggle="modal" data-bs-target="#admin_edit_user" data-bs-whatever="@mdo" id="${data2.id}"></i>
+                        <i class="fa fa-cogs" aria-hidden="true"  data-bs-toggle="modal" data-bs-target="#admin_edit_user" data-bs-whatever="@mdo" id="edit-user-${data2.id}"></i>
                     </button>
                     <button class="button-icon">
-                        <i class="fa fa-trash" aria-hidden="true"  data-bs-toggle="modal" data-bs-target="#admin_create_new_" data-bs-whatever="@mdo" id="${data2.id}" ></i>
+                        <i class="fa fa-trash" aria-hidden="true"  data-bs-toggle="modal" data-bs-target="#admin_delete_user" data-bs-whatever="@mdo" id="delete-user-${data2.id}" ></i>
                     </button>
                 </td>
             </tr>
             `
         
         })
-        document.getElementById("user-table").innerHTML = `${a.join("")}`
+        document.getElementById("user-table").innerHTML = `${a.join("")}`;
+        
+        // EDIT USER
+        for (let index of data){
+            const userId=index.id;
+            // lắng nghe sự kiện update
+            document.getElementById("edit-user-"+userId).addEventListener("click",()=>{
+                // lấy giá trị gắn cho value
+                document.getElementById("admin_edit_user_id").value=userId;
+                document.getElementById("admin_edit_user_name").value=index.name;
+                document.getElementById("admin_edit_user_email").value=index.email;
+                document.getElementById("admin_edit_user_phone").value=index.phone;
+                document.getElementById("admin_edit_user_password").value=index.password;
+                document.getElementById("admin_edit_user_gender").value=index.gender;
+                document.getElementById("admin_edit_user_date_birth").value=index.age;
+                document.getElementById("admin_edit_user_id").setAttribute("disabled","disabled");
+                document.getElementById("admin_edit_form_user").addEventListener("click",()=>{
+                    var id=document.getElementById("admin_edit_user_id").value;
+                    var name=document.getElementById("admin_edit_user_name").value;
+                    var email=document.getElementById("admin_edit_user_email").value;
+                    var phone=document.getElementById("admin_edit_user_phone").value;
+                    var password=document.getElementById("admin_edit_user_password").value;
+                    var birthday=document.getElementById("admin_edit_user_date_birth").value;
+                    var gender=document.getElementById("admin_edit_user_gender").value;
+    
+                    const editUser= {
+                        id:userId,
+                        name:name,
+                        email: email,
+                        phone: phone,
+                        password: password,
+                        avata:"https://img4.thuthuatphanmem.vn/uploads/2020/08/27/anh-avatar-zalo-cute-nhat-cho-con-trai_052907656.jpg",
+                        age: birthday,
+                        gender:gender
+                    };
+                    console.log("NHÌN GÌ MÀ NHÌNH"+editUser);
+                    // up lên server
+                    fetch(`http://localhost:3000/user/${index.id}`, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(editUser),
+                    })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error("Cập nhật User thất bại.");
+                        }
+                        return response.json();
+                    })
+                    .then((editUser) => {
+                        // Xử lý phản hồi sau khi user đã được cập nhật thành công
+                        console.log("You have successfully added the user: ", editUser);
+                        alert("You have successfully edit the user");
+                        return;
+                        })
+                    .catch((error) => {
+                        // Xử lý lỗi nếu có
+                        console.error("Lỗi khi cập nhật user:", error);
+                    })
+                })
+            })
+            
+
+            // delete
+        document.getElementById("delete-user-" + userId).addEventListener("click",()=>{
+            document.getElementById("admin_ok_delete_user").addEventListener("click",()=>{
+                fetch(`http://localhost:3000/user/${userId}`, {
+                method: "DELETE",
+                })
+                .then((response) => {
+                    if (!response.ok) {
+                    throw new Error("Xóa người dùng thất bại.");
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    // Xử lý phản hồi từ json sau khi xóa thành công
+                    console.log("User đã được xóa:", data);
+                    alert("You have successfully delete the user")
+                    return;
+                    // Cập nhật giao diện hoặc làm bất kỳ điều gì bạn muốn sau khi xóa thành công
+                })
+                .catch((error) => {
+                    console.error("Lỗi khi xóa user:", error);
+                });
+            })
+            
+        })
+        }
+        
     })
-    // 
+    // tạo user
+    function createUser(){
+        var name=document.getElementById("admin_create_user_name").value;
+        var email=document.getElementById("admin_create_user_email").value;
+        var phone=document.getElementById("admin_create_user_phone").value;
+        var password=document.getElementById("admin_create_user_password").value;
+        var confirmPassword=document.getElementById("admin_create_user_confirm_password").value;
+        var birthday=document.getElementById("admin_create_user_birthday").value;
+        var gender=document.getElementById("admin_create_user_gender").value;
+        // lỗi trống
+        if (name==""||email==""||password==""||confirmPassword==""||birthday==""||gender==""){
+            alert("Please provide complete information");
+            return ;
+        }else if (password == confirmPassword){
+            // tạo đối tượng chứa dữ liệu đăng kí
+            const formDataUser={
+                name:name,
+                email: email,
+                phone: phone,
+                password: password,
+                avata:"https://img4.thuthuatphanmem.vn/uploads/2020/08/27/anh-avatar-zalo-cute-nhat-cho-con-trai_052907656.jpg",
+                age: birthday,
+                gender:gender
+            }
+            console.log(formDataUser)
+            // gửi dữ liệu lên json server
+            fetch(user,{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(formDataUser)
+            })
+            .then(response=>{
+                if (response.ok){
+                    alert("You have successfully added the user")
+                return;
+                }
+                else {
+                    alert("Password don't match retype your Password.");
+                    return;
+                }
+            })
+
+        }
+    }
