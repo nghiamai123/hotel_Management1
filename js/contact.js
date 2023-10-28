ae65ded77610115d74fa7763d6612ed2a7c49269
Duong Thi Hong Lam <lam.duong25@student.passerellesnumeriques.org>
2023-10-28 19:32:56 +0700

    finish contact page

--- js/contact.js
+++ js/contact.js
@@ -1,27 +1,25 @@
-function contactViaEmail() {
-    var recipient = "lam.duong25@student.passerellesnumeriques.org";  //tạo mặc định người nhận
-    var ccRecipients = ["vietmy@gmail.com", "xuom@gmail.com","nghiamai@gmail.com"]; //tạo mặc định cc
-    var mailtoUrl = "mailto:" + recipient + "?cc=" + ccRecipients.join(",");
-    window.location.href = mailtoUrl;
-}
-// send Email
-function sendEmail() { 
-  var fullName = document.getElementById("fullName").value;
-  console.log(fullName);
-  var email = document.getElementById("email").value;
-  console.log(email);
-  var phoneNumber = document.getElementById("phoneNumber").value;
-  var textDescription = document.getElementById("textDescription").value;
-
-  if (fullName == "" || email == "" || phoneNumber == "" || textDescription == "") {
-    alert("Please re-enter");
-    return;
-  }
-
-  var recipientEmail = "lam.duong25@student.passerellesnumeriques.org";
-  var ccRecipients = ["vietmy@gmail.com", "xuom@gmail.com", "nghiamai@gmail.com"];
-  var subject = "Contact";
-  var body = "Dear Yawn Yawn hotel,\n\nMy name is " + fullName + "\n" + textDescription;
+// function contactViaEmail() {
+//     var recipient = "lam.duong25@student.passerellesnumeriques.org";  //tạo mặc định người nhận
+//     var ccRecipients = ["vietmy@gmail.com", "xuom@gmail.com","nghiamai@gmail.com"]; //tạo mặc định cc
+//     var mailtoUrl = "mailto:" + recipient + "?cc=" + ccRecipients.join(",");
+//     window.location.href = mailtoUrl;
+// }
+// contact via email
+function contactViaEmail() { 
+  fetch(listUser1)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(element => {
+      if (userid == element.id){
+        if (JSON.parse(localStorage.getItem("currentlylogin"))){ //khi chưa đăng nhập thì các ô dữ liệu trống
+        var fullName = element.name;
+        var email = element.email;
+        var phoneNumber= element.phone;
+      }}
+  var recipientEmail = "hoahauhk@gmail.com";
+  var ccRecipients = ["lam.duong25@student.passerellesnumeriques.org", "honglamhk2004@gmail.com"];
+  var subject = "[Contact]";
+  var body = "Dear Yawn Yawn hotel,\nMy name is "+fullName  + "\n connect me via" + email + " or phone number " + phoneNumber ;
 
   var mailtoUrl = "mailto:" + recipientEmail + "?cc=" + ccRecipients.join(",") +
     "&subject=" + encodeURIComponent(subject) +
@@ -29,28 +27,25 @@ function sendEmail() {
     "&from=" + encodeURIComponent(email);
   window.location.href = mailtoUrl;
 }
+)})};
 
 
-
-
-const listUser1 = "http://localhost:3000/user";
+const listUser1 = "http://localhost:3000/user"; //show dữ liệu người dùng ra các ô input
 var userid = localStorage.getItem('listuser');
 fetch(listUser1)
 .then((res) => res.json())
 .then((data) => {
     data.forEach(element => {
       if (userid == element.id){
-        if (JSON.parse(localStorage.getItem("currentlylogin"))){
+        if (JSON.parse(localStorage.getItem("currentlylogin"))){ //khi chưa đăng nhập thì các ô dữ liệu trống
         document.getElementById("fullName").value = element.name;
-        console.log(element.name);
         document.getElementById("email").value = element.email;
-        console.log(element.email);
         document.getElementById("phoneNumber").value = element.phone;
-        console.log(element.phone);
       }}
     }
     )
   });
+
 
 // submit Contact Form
 	function submitContactForm() { 
